8a7c6687b67039bdb3700ed91ebe1b4df7b99c33
carlos373 <carlos.ivo@aluno.senai.br>
2024-09-05 09:12:04 -0300

    Add files via upload

---
+++ prova1.js
@@ -0,0 +1,19 @@
+let chi, ano
+
+do{
+    chi = parseInt(prompt("Qual o numero de chinchilas?"))
+    if(isNaN(chi) || chi >2){
+        alert("Numero invalido, Digite um outro valor maior ou igual a 2")
+    }
+    ano = parseInt(prompt("Qual o ano que você está?"))
+    
+    ano = chi * 3
+    document.write("")
+}while (isNaN(n1) || n1 < 0)
+
+
+// 1º Elabore um programa que realize a leitura de um número e, caso o número for par ele exibe o dobro do número e se for ímpar, o tripl. A leitura continua até que o usuário infrome 0 (ou algum valor inválido). Inicialmente, uma mensagem alertando sobre a execução do programa é apresentada
+
+// 2º Elabore um program que faz a leitura de contas que devem ser pagas por um usuário. As contas são exibidas e no final da listagem o número de contas(contador) e a soma dos valores (acumulador) são destacados.
+
+// 3º Digamos que o número de chinchilas de uma fazenda triplica a cada ano, após o primeiro ano. Elaborar um programa que leia o número inicial de chinchilas e anos e informe ano a nao o número médio previsto de chinchilas da fazend. Validar a entrada para que o numero inicial de chinchilas seja maior que u igual a 2.
